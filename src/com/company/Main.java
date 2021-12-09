c875dda8bd5da6de08699ae0a6a75d35115e5aed
AvaluevGH <anton.valuev.99@mail.ru>
2021-12-10 00:42:51 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Potatoes potatoes1 = new Potatoes("Фри", 150);
+        Potatoes potatoes2 = new Potatoes("Варенная", 80);
+        Potatoes potatoes3 = new Potatoes("Жаренная", 100);
+        potatoes1.system_print();
+        potatoes2.system_print();
+        potatoes3.system_print();
+        Potatoes[] breakfast = new Potatoes[6];
+
+
+
+
+
+    }
+}
---
+++ src/com/company/Potatoes.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Potatoes  {
+    int  calories;
+    String type;
+
+    public Potatoes (String potatoesType, int potatoesCalories)
+    {
+        this.type=potatoesType;
+        this.calories=potatoesCalories;
+    }
+    public void system_print(){
+        System.out.println("Тип:"+ type+", калории: "+calories);
+
+    }
+
+
+}
