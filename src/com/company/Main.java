79b6f3a23fec9ca44c8b80b3498eb55f02f41b48
AvaluevGH <anton.valuev.99@mail.ru>
2021-12-10 00:46:48 +0300

    Add: counting products in breakfast

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,11 +11,35 @@ public class Main {
         potatoes1.system_print();
         potatoes2.system_print();
         potatoes3.system_print();
+
         Potatoes[] breakfast = new Potatoes[6];
+        breakfast[0] = new Potatoes("Фри", 150  );
+        breakfast[1] = new Potatoes("Варенная", 80);
+        breakfast[2] = new Potatoes("Жаренная", 100);
+        breakfast[3] = new Potatoes("Варенная", 50);
+        breakfast[4] = new Potatoes("Фри", 150);
+        breakfast[5] = new Potatoes("Фри", 200);
 
-
-
-
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        for (Potatoes potatoes : breakfast) {
+            if (Objects.equals(potatoes.type, "Варенная")) {
+                count1++;
+            }
+        }
+        for (Potatoes potatoes : breakfast) {
+            if (Objects.equals(potatoes.type, "Жаренная")) {
+                count2++;
+            }
+        }
+        for (Potatoes potatoes : breakfast) {
+            if (Objects.equals(potatoes.type, "Фри")) {
+                count3++;
+            }
+        }
+        System.out.println("Завтрак состоит из "+ count1+" Варенной, "+count2+" Жаренной, "+count3+" Фри Картошки");
+        int cal = 0;
 
     }
 }
